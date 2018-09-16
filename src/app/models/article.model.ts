d002da1f3b4cb40436fcc3db7bef540fe96f5286
Nicholas Brun <nicholasb@hotmail.fr>
2018-09-16 15:20:34 +0200

    Feat(univers): add articles templates

--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -12,6 +12,43 @@ export class Article {
 
 export const articles = [
   {
-    "name": "Alphonse"
+    "title": "La Création",
+    "type": "Religion",
+    "description": "Selon la légende connue par tous, la divinité de la Vie et de la Mort, Arlénor, a façonné le monde..."
+  },
+  {
+    "title": "La Magie des cristaux",
+    "type": "Technologie",
+    "description": "La magie du Monde d'Arlénor provient des cristaux laissés par les Anges et les Démons lors de la Création..."
+  },
+  {
+    "title": "La Création",
+    "type": "Religion",
+    "description": "Selon la légende connue par tous, la divinité de la Vie et de la Mort, Arlénor, a façonné le monde..."
+  },
+  {
+    "title": "La Magie des cristaux",
+    "type": "Technologie",
+    "description": "La magie du Monde d'Arlénor provient des cristaux laissés par les Anges et les Démons lors de la Création..."
+  },
+  {
+    "title": "La Création",
+    "type": "Religion",
+    "description": "Selon la légende connue par tous, la divinité de la Vie et de la Mort, Arlénor, a façonné le monde..."
+  },
+  {
+    "title": "La Magie des cristaux",
+    "type": "Technologie",
+    "description": "La magie du Monde d'Arlénor provient des cristaux laissés par les Anges et les Démons lors de la Création..."
+  },
+  {
+    "title": "La Création",
+    "type": "Religion",
+    "description": "Selon la légende connue par tous, la divinité de la Vie et de la Mort, Arlénor, a façonné le monde..."
+  },
+  {
+    "title": "La Magie des cristaux",
+    "type": "Technologie",
+    "description": "La magie du Monde d'Arlénor provient des cristaux laissés par les Anges et les Démons lors de la Création..."
   }
 ]
