41c533a2acd1f4059b122e653fb62d5387604217
Nicholas Brun <nicholasb@hotmail.fr>
2018-09-23 15:03:12 +0200

    Fix(header): Change the header with the location

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public route:string;
+
+  constructor(location: Location, router: Router) {
+    router.events.subscribe((val) => {
+       var currentLocation = location.path();
+       this.route = currentLocation.split("/")[1];
+    });
+  }
 
   ngOnInit() {
   }
