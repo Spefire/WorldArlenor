3ac81e9cb146f4f900c954cabd0a5293504bb739
Nicholas Brun <nicholasb@hotmail.fr>
2018-09-25 22:01:02 +0200

    feat(universe): prepare the hover on the map

--- src/app/pages/universe/universe.component.ts
+++ src/app/pages/universe/universe.component.ts
@@ -21,4 +21,35 @@ export class UniverseComponent implements OnInit {
     this.listFacts = this.apiService.getFacts();
   }
 
+  getCoords() {
+    var img = document.getElementById('map-arlenor');
+    var width = img.clientWidth;
+    var base = [
+      {x: 545, y: 840},
+      {x: 615, y: 1105},
+      {x: 410, y: 1090},
+      {x: 405, y: 1055},
+      {x: 240, y: 1120},
+      {x: 50, y: 965},
+      {x: 90, y: 820},
+      {x: 270, y: 745},
+      {x: 340, y: 810},
+      {x: 300, y: 890},
+      {x: 355, y: 935},
+      {x: 490, y: 910}
+    ];
+    var final = "";
+    var isfirst = true;
+    var coeff = width / 1800;
+    base.forEach(element => {
+      if (isfirst) {
+        isfirst = false;
+        final += element.x * coeff + "," + element.y * coeff;
+      }
+      else {
+        final += "," + element.x * coeff + "," + element.y * coeff;
+      }
+    });
+    return final;
+  }
 }
