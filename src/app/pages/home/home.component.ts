375c0c02119475e8ef286b03ce7cd0a59a898518
Nicholas Brun <nicholasb@hotmail.fr>
2018-10-07 17:35:17 +0200

    Style(home): Fix the transition of sliders

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval } from 'rxjs';
 import { RoutesService } from '../../services/routes.service';
 
 @Component({
@@ -7,16 +8,38 @@ import { RoutesService } from '../../services/routes.service';
   styleUrls: ['./home.component.scss'],
   providers: [RoutesService]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  private selection:string;
+	private sub:any;
+  private selection:number;
 
   constructor(private routesService: RoutesService) {
     this.routesService.setTitleMetas("HOME");
-    this.selection = "universe";
-  }
+	}
 
-  changeSelection(newSelection:string) {
-    this.selection = newSelection;
+	ngOnInit() {
+		this.selection = 0;
+		this.sub = interval(10000).subscribe((val) => {
+			this.changeSelection();
+		});
+	}
+
+	changeSelection() {
+		console.log("CHANGE");
+		if (this.selection == 0) this.selection = 1;
+		else if (this.selection == 1) this.selection = 2;
+		else if (this.selection == 2) this.selection = 0;
+	}
+
+  setSelection(newSelection:number) {
+		this.selection = newSelection;
+		this.sub.unsubscribe();
+		this.sub = interval(10000).subscribe((val) => {
+			this.changeSelection();
+		});
+	}
+
+	ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
