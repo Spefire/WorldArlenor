5a432cebd493db1a3419c28b607513e19856e91d
Nicholas Brun <spefire@gmail.com>
2018-10-23 17:39:47 +0200

    Feat(religion): Can select kabal and prepare the json for entity text

--- src/app/pages/universe/religion/religion.component.ts
+++ src/app/pages/universe/religion/religion.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { RoutesService } from '../../../services/routes.service';
 
 @Component({
@@ -10,14 +11,27 @@ import { RoutesService } from '../../../services/routes.service';
 })
 export class ReligionComponent {
 
+	public entityKabal:string;
+	public entityTitle:string;
+	public entityComment:string;
+	public entityCommentName:string;
 	public select:number;
 
-  constructor(private routesService: RoutesService, private route: ActivatedRoute) {
+  constructor(private routesService: RoutesService, private route: ActivatedRoute, private translate: TranslateService) {
 		this.routesService.setTitleMetas("RELIGION");
-		this.select = 1;
+		this.changeSelection(1);
 	}
 
 	changeSelection(newSelection:number) {
 		this.select = newSelection;
+		if (this.select <= 10) {
+			this.translate.get('UNIVERSE.RELIGION.LIGHTKABAL').subscribe((res: string) => {
+				this.entityKabal = this.translate.instant(res);
+			});
+		} else {
+			this.translate.get('UNIVERSE.RELIGION.DARKKABAL').subscribe((res: string) => {
+				this.entityKabal = this.translate.instant(res);
+			});
+		}
 	}
 }
