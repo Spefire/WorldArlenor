ebe831e2df6a7c9cf3fdcb6d6e421baee9bb389c
Nicholas Brun <spefire@gmail.com>
2018-11-06 13:33:20 +0100

    Refactor(facts): Prepare the structure for adding images

--- src/app/models/fact.model.ts
+++ src/app/models/fact.model.ts
@@ -17,32 +17,42 @@ export class Fact {
 export const facts = [
   {
     "id": 0,
-    "title": "La Cité de Faradel",
-    "zone": "Faradel",
+		"title": "La Cité de Faradel",
+		"zone": "Faradel",
+		"logoSrc": "https://via.placeholder.com/200x200",
+		"backgroundSrc": "https://via.placeholder.com/1000x400",
     "description": "La magie du Monde d'Arlénor provient des cristaux laissés par les Anges et les Démons lors de la Création..."
   },
   {
     "id": 1,
     "title": "La Tour du Savoir",
-    "zone": "Faradel",
+		"zone": "Faradel",
+		"logoSrc": "https://via.placeholder.com/200x200",
+		"backgroundSrc": "https://via.placeholder.com/1000x400",
     "description": "Selon la légende connue par tous, la divinité de la Vie et de la Mort, Arlénor, a façonné le monde..."
   },
   {
     "id": 2,
     "title": "Le Commerce de Jirakan",
-    "zone": "Jirakan",
+		"zone": "Jirakan",
+		"logoSrc": "https://via.placeholder.com/200x200",
+		"backgroundSrc": "assets/images/facts/jirakan.jpg",
     "description": "La magie du Monde d'Arlénor provient des cristaux laissés par les Anges et les Démons lors de la Création..."
   },
   {
     "id": 3,
     "title": "Le Grand Massacre",
-    "zone": "Jirakan",
+		"zone": "Jirakan",
+		"logoSrc": "https://via.placeholder.com/200x200",
+		"backgroundSrc": "assets/images/facts/massacre.jpg",
     "description": "Selon la légende connue par tous, la divinité de la Vie et de la Mort, Arlénor, a façonné le monde..."
   },
   {
     "id": 4,
     "title": "L'Ile de Lumeck",
-    "zone": "Shivazen",
+		"zone": "Shivazen",
+		"logoSrc": "https://via.placeholder.com/200x200",
+		"backgroundSrc": "https://via.placeholder.com/1000x400",
     "description": "La magie du Monde d'Arlénor provient des cristaux laissés par les Anges et les Démons lors de la Création..."
 	}
 ]
