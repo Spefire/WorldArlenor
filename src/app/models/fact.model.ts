93d24e1fd15a71fc5056df2b5acb01390a2936f2
Nicholas Brun <spefire@gmail.com>
2018-11-06 16:50:34 +0100

    Refactor(facts): Change for background of facts

--- src/app/models/fact.model.ts
+++ src/app/models/fact.model.ts
@@ -19,23 +19,23 @@ export const facts = [
     "id": 0,
 		"title": "La Cité de Faradel",
 		"zone": "Faradel",
-		"logoSrc": "https://via.placeholder.com/200x200",
-		"backgroundSrc": "https://via.placeholder.com/1000x400",
+		"logoSrc": "assets/images/facts/icon.png",
+		"backgroundSrc": "assets/images/facts/faradel.jpg",
     "description": "La magie du Monde d'Arlénor provient des cristaux laissés par les Anges et les Démons lors de la Création..."
   },
   {
     "id": 1,
     "title": "La Tour du Savoir",
 		"zone": "Faradel",
-		"logoSrc": "https://via.placeholder.com/200x200",
-		"backgroundSrc": "https://via.placeholder.com/1000x400",
+		"logoSrc": "assets/images/facts/icon.png",
+		"backgroundSrc": "assets/images/facts/tour.jpg",
     "description": "Selon la légende connue par tous, la divinité de la Vie et de la Mort, Arlénor, a façonné le monde..."
   },
   {
     "id": 2,
     "title": "Le Commerce de Jirakan",
 		"zone": "Jirakan",
-		"logoSrc": "https://via.placeholder.com/200x200",
+		"logoSrc": "assets/images/facts/icon.png",
 		"backgroundSrc": "assets/images/facts/jirakan.jpg",
     "description": "La magie du Monde d'Arlénor provient des cristaux laissés par les Anges et les Démons lors de la Création..."
   },
@@ -43,7 +43,7 @@ export const facts = [
     "id": 3,
     "title": "Le Grand Massacre",
 		"zone": "Jirakan",
-		"logoSrc": "https://via.placeholder.com/200x200",
+		"logoSrc": "assets/images/facts/icon.png",
 		"backgroundSrc": "assets/images/facts/massacre.jpg",
     "description": "Selon la légende connue par tous, la divinité de la Vie et de la Mort, Arlénor, a façonné le monde..."
   },
@@ -51,8 +51,8 @@ export const facts = [
     "id": 4,
     "title": "L'Ile de Lumeck",
 		"zone": "Shivazen",
-		"logoSrc": "https://via.placeholder.com/200x200",
-		"backgroundSrc": "https://via.placeholder.com/1000x400",
+		"logoSrc": "assets/images/facts/icon.png",
+		"backgroundSrc": "assets/images/facts/faradel.jpg",
     "description": "La magie du Monde d'Arlénor provient des cristaux laissés par les Anges et les Démons lors de la Création..."
 	}
 ]
