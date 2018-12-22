f9c0f89a11475e827223c8ccf621e2f50a969676
Nicholas Brun <nicholasb@hotmail.fr>
2018-12-22 20:15:17 +0100

    Feat : Add scroll for the home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,30 +11,59 @@ import { RoutesService } from '../../services/routes.service';
 export class HomeComponent implements OnInit, OnDestroy {
 
 	private sub:any;
-  private selection:number;
+	private selection:number;
+	private scrollValue:number;
 
   constructor(private routesService: RoutesService) {
-    this.routesService.setTitleMetas("HOME");
+		this.routesService.setTitleMetas("HOME");
+		window.addEventListener('mousewheel', this.mouseWheelEvent.bind(this));
+		window.addEventListener('DOMMouseScroll', this.mouseWheelEvent.bind(this));
+	}
+
+	mouseWheelEvent(e) {
+		this.scrollValue += e.wheelDelta ? e.wheelDelta : -e.detail;
+		if (this.scrollValue >= 360) {
+			this.scrollValue = -120;
+			this.decreaseSelection();
+			this.sub.unsubscribe();
+			this.sub = interval(8000).subscribe((val) => {
+				this.increaseSelection();
+			});
+		} else if (this.scrollValue <= -360) {
+			this.scrollValue = 120;
+			this.increaseSelection();
+			this.sub.unsubscribe();
+			this.sub = interval(8000).subscribe((val) => {
+				this.increaseSelection();
+			});
+		}
 	}
 
 	ngOnInit() {
 		this.selection = 0;
+		this.scrollValue = 0;
 		this.sub = interval(8000).subscribe((val) => {
-			this.changeSelection();
+			this.increaseSelection();
 		});
 	}
 
-	changeSelection() {
+	increaseSelection() {
 		if (this.selection == 0) this.selection = 1;
 		else if (this.selection == 1) this.selection = 2;
 		else if (this.selection == 2) this.selection = 0;
+	}
+
+	decreaseSelection() {
+		if (this.selection == 0) this.selection = 2;
+		else if (this.selection == 1) this.selection = 0;
+		else if (this.selection == 2) this.selection = 1;
 	}
 
   setSelection(newSelection:number) {
 		this.selection = newSelection;
 		this.sub.unsubscribe();
 		this.sub = interval(8000).subscribe((val) => {
-			this.changeSelection();
+			this.increaseSelection();
 		});
 	}
 
