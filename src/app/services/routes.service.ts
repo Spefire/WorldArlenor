cf2afa97aea229a441958be95af48fa69122b8e5
Nicholas Brun <nicholasb@hotmail.fr>
2018-12-29 13:24:21 +0100

    Refactor : Change page titles

--- src/app/services/routes.service.ts
+++ src/app/services/routes.service.ts
@@ -8,7 +8,7 @@ export class RoutesService {
   constructor(private titleService: Title, private meta: Meta, private translate: TranslateService) {}
 
   public setTitleMetas(pageName: string) {
-    this.translate.get('PAGE.' + pageName + '.TITLE').subscribe((res: string) => {
+    this.translate.get('PAGE.' + pageName).subscribe((res: string) => {
 			this.titleService.setTitle(this.translate.instant(res));
 			this.meta.updateTag({
         property: 'og:title',
