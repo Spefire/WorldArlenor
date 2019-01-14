184d6db00290099a6677fbcc7675c76b68520f60
Nicholas Brun <spefire@gmail.com>
2019-01-14 23:03:18 +0100

    Feat(creation): Prepare for caracteristics

--- src/app/pages/roleplay/creation/creation.component.ts
+++ src/app/pages/roleplay/creation/creation.component.ts
@@ -10,13 +10,70 @@ import jsPDF from 'jspdf';
 })
 export class CreationComponent {
 
+	public name:string;
+	public race:string;
+	public caracteristics:any;
+	public mainSkills:any;
+	public crystalSkills:any;
+
+	public leftPointsCaracteristics:number;
+	public leftPointsSkills:number;
+
   constructor(private routesService: RoutesService) {
-    this.routesService.setTitleMetas("CREATION");
-  }
+		this.routesService.setTitleMetas("CREATION");
+		this.caracteristics = {
+			vigueur : 1,
+			habilete : 1,
+			intellect : 1,
+			presence : 1,
+			pouvoir : 1,
+		};
+		this.mainSkills = {
+			animaux : 1,
+			art : 1,
+			artisanat : 1,
+			athletisme : 1,
+			combat : 1,
+			etiquette : 1,
+			furtivite : 1,
+			information : 1,
+			intuition : 1,
+			investigation : 1,
+			larcin : 1,
+			manipulation : 1,
+			medecine : 1,
+			persuasion : 1,
+			pilotage : 1,
+			savoir : 1,
+			survie : 1
+		};
+		this.crystalSkills = {
+			elementaires : 1,
+			invocations : 1,
+			mentaux : 1,
+			transformation : 1,
+			spatiauxtemporels : 1,
+			speciaux : 1
+		};
+		this.refreshPoints();
+	}
+	
+	test(type, value) {
+		this.caracteristics[type] = parseInt(value);
+		this.refreshPoints();
+	}
+
+	refreshPoints() {
+		var totalCaracteristics = 0;
+		for (var key in this.caracteristics) {
+			totalCaracteristics += this.caracteristics[key];
+		}
+		this.leftPointsCaracteristics = 15 - totalCaracteristics;
+	}
 
 	downloadPDF() {
 		var doc = new jsPDF();
-		doc.setFontSize(40);
+		doc.setFontSize(30);
 		doc.text(35, 25, 'Fiche de personnage');
 
 		this.toDataURL('./assets/images/creation/Fond.png', function(dataUrl) {
