3cbc766f6ff22227afff4ed96a90270fb64aa9d1
Nicholas Brun <spefire@gmail.com>
2019-01-14 14:11:32 +0100

    Feat(creation): Add functions to upload PDF with an image and a text

--- src/app/pages/roleplay/creation/creation.component.ts
+++ src/app/pages/roleplay/creation/creation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RoutesService } from '../../../services/routes.service';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-creation',
@@ -7,13 +8,35 @@ import { RoutesService } from '../../../services/routes.service';
   styleUrls: ['./creation.component.scss'],
   providers: [RoutesService]
 })
-export class CreationComponent implements OnInit {
+export class CreationComponent {
 
   constructor(private routesService: RoutesService) {
     this.routesService.setTitleMetas("CREATION");
   }
 
-  ngOnInit() {
-  }
+	downloadPDF() {
+		var doc = new jsPDF();
+		doc.setFontSize(40);
+		doc.text(35, 25, 'Fiche de personnage');
 
+		this.toDataURL('./assets/images/creation/Fond.png', function(dataUrl) {
+			console.log('RESULT:', dataUrl);
+			doc.addImage(dataUrl, 'JPEG', 15, 40, 180, 160);
+			doc.save("a4.pdf");
+		})
+	}
+
+	toDataURL(url, callback) {
+		var xhr = new XMLHttpRequest();
+		xhr.onload = function() {
+			var reader = new FileReader();
+			reader.onloadend = function() {
+				callback(reader.result);
+			}
+			reader.readAsDataURL(xhr.response);
+		};
+		xhr.open('GET', url);
+		xhr.responseType = 'blob';
+		xhr.send();
+	}
 }
