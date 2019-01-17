8e2bac7dd5d071b317b2a7566205aa2407eef095
Nicholas Brun <spefire@gmail.com>
2019-01-17 17:39:28 +0100

    Feat(creation): Prepare text help via fr json

--- src/app/components/creation-slider/creation-slider.component.ts
+++ src/app/components/creation-slider/creation-slider.component.ts
@@ -8,16 +8,23 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class CreationSliderComponent {
 
 	@Input() public name:string;
+	@Input() public help:string;
 	@Input() public type:string;
 	@Input() public value:number;
 	@Input() public min:number;
 	@Input() public max:number;
 	@Output() pointsValueChange = new EventEmitter();
 
+	public displayHelp: boolean;
+
 	constructor() {}
-	
+
 	valueChanged(value) {
 		this.pointsValueChange.emit({type : this.type, value : value});
+	}
+
+	toogleHelp() {
+		this.displayHelp = !this.displayHelp;
 	}
 
 }
