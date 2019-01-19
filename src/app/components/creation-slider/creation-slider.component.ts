febbe6bf76479e5f8bb079b83cd9bccb428435d7
Nicholas Brun <spefire@gmail.com>
2019-01-19 17:57:03 +0100

    Feat(creationslider): Change value in real time

--- src/app/components/creation-slider/creation-slider.component.ts
+++ src/app/components/creation-slider/creation-slider.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, AfterViewInit, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'creation-slider',
   templateUrl: './creation-slider.component.html',
   styleUrls: ['./creation-slider.component.scss']
 })
-export class CreationSliderComponent {
+export class CreationSliderComponent implements AfterViewInit {
 
 	@Input() public name:string;
 	@Input() public help:string;
@@ -16,15 +16,23 @@ export class CreationSliderComponent {
 	@Output() pointsValueChange = new EventEmitter();
 
 	public displayHelp: boolean;
+	public rangeSlider: any;
+	public rangeBullet: any;
 
-	constructor() {}
+	constructor(private elementRef:ElementRef) {
+	}
 
-	valueChanged(value) {
-		this.pointsValueChange.emit({type : this.type, value : value});
+	ngAfterViewInit() {
+		this.rangeSlider = this.elementRef.nativeElement.querySelector("#rs-range-line");
+		this.rangeBullet = this.elementRef.nativeElement.querySelector("#rs-bullet");
+		this.rangeSlider.addEventListener("input", this.valueChange.bind(this));
 	}
 
 	toogleHelp() {
 		this.displayHelp = !this.displayHelp;
 	}
 
+	valueChange() {
+		this.pointsValueChange.emit({type : this.type, value : this.rangeSlider.value});
+	}
 }
