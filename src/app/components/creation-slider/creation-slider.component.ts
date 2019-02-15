037a8bc11bbceacb3d20f617fb16f31bc08856b3
Nicholas Brun <spefire@gmail.com>
2019-02-15 22:39:26 +0100

    Refactor(creation): Set two columns and remove input range

--- src/app/components/creation-slider/creation-slider.component.ts
+++ src/app/components/creation-slider/creation-slider.component.ts
@@ -1,25 +1,29 @@
-import { Component, Input, Output, EventEmitter, AfterViewInit, ElementRef} from '@angular/core';
+import { Component, Input, Output, EventEmitter, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'creation-slider',
-  templateUrl: './creation-slider.component.html',
-  styleUrls: ['./creation-slider.component.scss']
+	selector: 'creation-slider',
+	templateUrl: './creation-slider.component.html',
+	styleUrls: ['./creation-slider.component.scss']
 })
 export class CreationSliderComponent implements AfterViewInit {
 
-	@Input() public name:string;
-	@Input() public help:string;
-	@Input() public type:string;
-	@Input() public value:number;
-	@Input() public min:number;
-	@Input() public max:number;
+	@Input() public name: string;
+	@Input() public help: string;
+	@Input() public type: string;
+	@Input() public value: number;
+	@Input() public min: number;
+	@Input() public max: number;
 	@Output() pointsValueChange = new EventEmitter();
 
+	public canUp: boolean;
+	public canDown: boolean;
 	public displayHelpClick: boolean;
 	public displayHelpHover: boolean;
 	public rangeSlider: any;
 
-	constructor(private elementRef:ElementRef) {
+	constructor(private elementRef: ElementRef) {
+		this.canUp = true;
+		this.canDown = false;
 	}
 
 	ngAfterViewInit() {
@@ -31,7 +35,25 @@ export class CreationSliderComponent implements AfterViewInit {
 		this.displayHelpClick = !this.displayHelpClick;
 	}
 
+	downValue() {
+		if (this.canDown) {
+			this.value--;
+			this.canUp = true;
+			if (this.value <= this.min) this.canDown = false;
+			this.valueChange();
+		}
+	}
+
+	upValue() {
+		if (this.canUp) {
+			this.value++;
+			this.canDown = true;
+			if (this.value >= this.max) this.canUp = false;
+			this.valueChange();
+		}
+	}
+
 	valueChange() {
-		this.pointsValueChange.emit({type : this.type, value : this.rangeSlider.value});
+		this.pointsValueChange.emit({ type: this.type, value: this.value });
 	}
 }
