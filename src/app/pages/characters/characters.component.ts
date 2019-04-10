c7abe57e6e4ca0cbe8f5691cbdd601c344b111a2
Nicholas Brun <spefire@gmail.com>
2019-04-10 22:09:16 +0200

    Feat(world3D): Add Unity WebGL

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RoutesService } from '../../services/routes.service';
 
+declare var UnityLoader;
+declare var UnityProgress;
+
 @Component({
 	selector: 'app-characters',
 	templateUrl: './characters.component.html',
@@ -9,11 +12,14 @@ import { RoutesService } from '../../services/routes.service';
 })
 export class CharactersComponent implements OnInit {
 
+	public gameInstance: any;
+
 	constructor(private routesService: RoutesService) {
 		this.routesService.setTitleMetas("CHARACTERS");
 	}
 
 	ngOnInit() {
+		this.gameInstance = UnityLoader.instantiate("gameContainer", "../../../assets/unity/Build/Builds.json", {onProgress: UnityProgress});
 	}
 
 }
